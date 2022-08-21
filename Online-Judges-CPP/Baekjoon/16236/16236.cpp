59f9f927086cb32ab69132566d9fc82f54db1f9a
reruo321 <reruo321@gmail.com>
2022-08-21 23:16:58 +0900

    committed (not working yet)

--- Online-Judges-CPP/Baekjoon/16236/16236.cpp
+++ Online-Judges-CPP/Baekjoon/16236/16236.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 
 using namespace std;
-
-class Point {
-	int x;
-	int y;
-};
+stack<pair<int, int>> coordiStack;
 
 class BabyShark {
 	int size;
@@ -21,21 +16,62 @@ public:
 		this->x = x;
 		this->y = y;
 	}
-	
+	void makeBigger() {
+		++size;
+	}
 	int getSize() {
 		return size;
 	}
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
 };
+
+void updateTime(int x, int y, int time, vector<vector<int>>& timeVector) {
+	// if the time at (x,y) is not updated or faster route is found, update.
+	if (timeVector.at(x).at(y) < 0 || timeVector.at(x).at(y) > time)
+		timeVector.at(x).at(y) = time;
+}
+
+// calculates reach time for all routes
+vector<vector<int>> calReachTime(BabyShark &shark, vector<vector<int>>& pool, vector<vector<int>>& timeVector, int n, int time = 1) {
+	int x = shark.getX();
+	int y = shark.getY();
+	int size = shark.getSize();
+	// go up
+	if (y > 0 && size >= pool.at(x).at(y - 1)) {
+		updateTime(x, y - 1, time, timeVector);
+		calReachTime(shark, pool, timeVector, n, time + 1);
+	}
+	// go left
+	if (x > 0 && size >= pool.at(x - 1).at(y)) {
+		updateTime(x - 1, y, time, timeVector);
+		calReachTime(shark, pool, timeVector, n, time + 1);
+	}
+	// go right
+	if (x < n - 1 && size >= pool.at(x + 1).at(y)) {
+		updateTime(x + 1, y, time, timeVector);
+		calReachTime(shark, pool, timeVector, n, time + 1);
+	}
+	// go down
+	if (y < n - 1 && size >= pool.at(x).at(y + 1)) {
+		updateTime(x, y + 1, time, timeVector);
+		calReachTime(shark, pool, timeVector, n, time + 1);
+	}
+}
 
 int main() {
 	int n;
 	int k;
-	int time = 0;
 
 	cin >> n;
 	vector<vector<int>> pool(n, vector<int>(n, 0));
-	stack<pair<int, int>> coordiStack;
+	vector<vector<int>> timeVector(n, vector<int>(n, -1));
 	BabyShark shark;
+	int sum = 0;
 
 	// gets pool info
 	for (int i = 0; i < n; ++i)
@@ -46,22 +82,39 @@ int main() {
 			else
 				pool.at(i).at(j) = k;
 		}
+	timeVector.at(shark.getX()).at(shark.getY()) = 0;
 
-	// finds edible fishes
 	while (1) {
+		int eatCount = 0;
+		int ftime = 0, fx, fy;
+		// calculates timeVector to know time for reaching every coordinate
+		calReachTime(shark, pool, timeVector, n);
+		
 		for(int i = 0; i < n; ++i)
 			for (int j = 0; j < n; ++j) {
-				// saves the coordinates of edible fishes
-				if (shark.getSize() > pool.at(i).at(j))
-					coordiStack.push(make_pair(i, j));
+				// finds edible fishes
+				if (pool.at(i).at(j) > 0 && shark.getSize() > pool.at(i).at(j)) {
+					// finds the minimum time to eat fish
+					if (ftime == 0 || ftime > timeVector.at(i).at(j)) {
+						ftime = timeVector.at(i).at(j);
+						fx = i;
+						fy = j;
+					}
+				}
 			}
-
+		// if there is an edible fish, move the shark and eat it
+		if (ftime > 0) {
+			shark.setCoordinate(fx, fy);
+			sum += ftime;
+			pool.at(fx).at(fy) = 0;
+			// if shark's size == the number of fish the shark has eaten, make him bigger
+			if (shark.getSize() == ++eatCount)
+				shark.makeBigger();
+		}
 		// if there is no edible fish, ends the program
-		if (coordiStack.empty()) {
-			cout << time;
+		else {
+			cout << sum;
 			return 0;
 		}
-
-
 	}
 }
