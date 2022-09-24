e4f048572b92483cf9eb76f5824640e74e46f7cd
reruo321 <reruo321@gmail.com>
2022-09-24 23:58:22 +0900

    committed

--- Online-Judges-CPP/_Algorithms/BFS/bfs.cpp
+++ Online-Judges-CPP/_Algorithms/BFS/bfs.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
 /*
 	BFS(Breadth First Search)
 
-
-
+	- Assumes All Vertices are Reachable from Start
+	- Checks Visited / Not Visited
+	- Uses Queue
 
 */
+
+class Graph {
+
+	// The Number of Vertex
+	int v;
+	// Adjacency Lists
+
+
+};
