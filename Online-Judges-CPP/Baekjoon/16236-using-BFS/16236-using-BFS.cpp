32d34e1e384feb7d4d38b0eb82126b2e81268f13
reruo321 <reruo321@gmail.com>
2022-09-26 23:52:03 +0900

    committed

--- Online-Judges-CPP/Baekjoon/16236-using-BFS/16236-using-BFS.cpp
+++ Online-Judges-CPP/Baekjoon/16236-using-BFS/16236-using-BFS.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/*
+	No. 16236 Using BFS
+
+	1. Spaces with (fish <= shark) size or without fish: BFS
+	2. Find the nearest space with small fish
+	3. Count the number the shark has eaten fishes -> Level Up
+
+*/
+
 int main() {
 	cout << "Test" << endl;
 	return 0;
