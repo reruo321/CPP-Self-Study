595721290d170e9d4c8f1d740cf38756b7a323d3
reruo321 <reruo321@gmail.com>
2022-10-31 23:59:18 +0900

    committed

--- Online-Judges-CPP/Baekjoon/14502/14502.cpp
+++ Online-Judges-CPP/Baekjoon/14502/14502.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void 
+
+void spreadVirus() {
+
+}
+
 int maxSafeZone() {
 	int n, m;
+	int wall, virus, min_virus = 64, wall_count;
+	vector<vector<int>> original_factory, factory_final;
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
+			cin >> original_factory.at(i).at(j);
+	
+	// Try to install three walls
+	while (1) {
+		wall_count = 0;
+
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < m; ++j) {
+
+			}
+
+		if(wall_count < 3)
+			break;
+	}
+
 }
 
 int main() {
