4edadadc725d1a5e4e616f16458d7591fbb5dd25
reruo321 <reruo321@gmail.com>
2022-11-09 18:26:10 +0900

    committed (finished)

--- Online-Judges-CPP/Baekjoon/14502/14502.cpp
+++ Online-Judges-CPP/Baekjoon/14502/14502.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 
 #define EMPTY 0
 #define WALL 1
@@ -9,96 +9,101 @@
 
 using namespace std;
 
-// produces all combinations for wall placement, and spreads virus for each case.
-int putWallspreadVirus(vector<vector<int>> &original_factory, const vector<pair<int, int>> & empty_list, queue<pair<int, int>>& virus_orin) {
-	// make combinations for wall placement	
-	pair<int, int> coord, new_coord;
-	int entity = WALL, max = 0, empty_count, virus_count;
-	vector<bool> bool_wallcombi(empty_list.size(), true);
-	// UP, LEFT, DOWN, RIGHT
-	int dr[] = { -1, 0, 1, 0 };
-	int dc[] = { 0, -1, 0, 1 };
+struct Coordinate {
+	int r;
+	int c;
+	Coordinate(): r(0), c(0) {}
+	Coordinate(int r, int c) : r(r), c(c) {}
+};
 
-	for (int i = 0; i < empty_list.size() - 3; ++i)
-		bool_wallcombi.at(i) = false;
+int maxEmptySpace() {
+	vector<vector<int>> factory, copied_factory;
+	queue<Coordinate> virus_list, copied_virus_list;
+	vector<Coordinate> combi_coord;
+	vector<bool> combi_bool;
+
+	int n, m, input;
+	int max = 0, count_empty;
+
+	cin >> n >> m;
 	
-	// combination loop
-	do {
-		// initialize factory and virus queue
-		vector<vector<int>> factory = original_factory;
-		queue<pair<int, int>> virus_queue = virus_orin;
-		empty_count = 0;
-		virus_count = 0;
+	// UP LEFT DOWN RIGHT
+	int dr[] = { -1, 0, 1, 0 };
+	int dc[] = { 0, -1, 0 , 1 };
 
-		for(int i=0; i < bool_wallcombi.size(); ++i)
-			// place walls
-			if (bool_wallcombi.at(i)) {
-				coord = empty_list.at(i);
-				factory.at(coord.first).at(coord.second) = WALL;
+	factory.assign(n, vector<int>(m));
+
+	// take input
+	for(int i=0; i<n; ++i)
+		for (int j = 0; j < m; ++j) {
+			cin >> input;
+			factory.at(i).at(j) = input;
+			switch (input) {
+			case EMPTY:
+				combi_coord.push_back(Coordinate(i, j));
+				break;
+			case WALL:
+				break;
+			case VIRUS:
+				virus_list.push(Coordinate(i, j));
+				break;
 			}
+		}
+
+	// ready wall combination
+	combi_bool.assign(combi_coord.size(), false);
+	for (int i = 0; i < 3; ++i)
+		combi_bool.at(i) = true;
+	sort(combi_bool.begin(), combi_bool.end());
+
+	do {
+		// initialize
+		copied_factory = factory;
+		copied_virus_list = virus_list;
+		count_empty = 0;
+
+		// select three empty spaces to place wall
+		for(int i=0; i<combi_bool.size(); ++i)
+			if (combi_bool.at(i)) {
+				Coordinate new_wall = combi_coord.at(i);
+				copied_factory.at(new_wall.r).at(new_wall.c) = WALL;
+			}
+		
 		// spread virus
-		while (!virus_queue.empty()) {
-			coord = virus_queue.front();
-			virus_queue.pop();
-			
-			// UP > LEFT > DOWN > RIGHT
+		while (!copied_virus_list.empty()) {
+			Coordinate virus = copied_virus_list.front();
+			copied_virus_list.pop();
+
+			// Go UP, LEFT, DOWN, RIGHT
 			for (int i = 0; i < 4; ++i) {
-				// UP DOWN
-				if (i % 2 == 0) {
-					if (coord.first + dr[i] > 0 && coord.first + dr[i] < factory.size()) {
-						new_coord = make_pair(coord.first + dr[i], coord.second);
-						entity = factory.at(new_coord.first).at(new_coord.second);
+				Coordinate new_virus = virus;
+				new_virus.r += dr[i];
+				new_virus.c += dc[i];
+
+				if (new_virus.r >= 0 && new_virus.c >= 0 && new_virus.r < n && new_virus.c < m) {
+					if (copied_factory.at(new_virus.r).at(new_virus.c) == EMPTY) {
+						copied_factory.at(new_virus.r).at(new_virus.c) = VIRUS;
+						copied_virus_list.push(new_virus);
 					}
-				}
-				// LEFT RIGHT
-				else {
-					if (coord.second + dc[i] > 0 && coord.second + dc[i] < factory.at(0).size()) {
-						new_coord = make_pair(coord.first, coord.second + dc[i]);
-						entity = factory.at(new_coord.first).at(new_coord.second);
-					}
-				}
-				// spread virus
-				if (entity == EMPTY) {
-					factory.at(new_coord.first).at(new_coord.second) = VIRUS;
-					virus_queue.push(new_coord);
 				}
 			}
 		}
-		for (auto& r : factory)
+
+		// find the number of empty spaces
+		for (auto& r : copied_factory)
 			for (auto& e : r)
 				if (e == EMPTY)
-					++empty_count;
+					++count_empty;
 
-		if (max < empty_count)
-			max = empty_count;
-	} while (next_permutation(bool_wallcombi.begin(), bool_wallcombi.end()));
-	
-	// return the answer
+		if (max < count_empty)
+			max = count_empty;
+
+	} while (next_permutation(combi_bool.begin(), combi_bool.end()));
+
 	return max;
 }
 
-int maxSafeZone() {
-	int n, m, input, wall_count = 0;
-	vector<vector<int>> original_factory;
-	vector<pair<int, int>> empty_list;
-	queue<pair<int, int>> virus_list;
-
-	cin >> n >> m;
-	original_factory.assign(n, vector<int>(m));
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j) {
-			cin >> input;
-			original_factory.at(i).at(j) = input;
-			if (input == EMPTY)
-				empty_list.push_back(make_pair(i, j));
-			else if (input == VIRUS)
-				virus_list.push(make_pair(i, j));
-			else
-				++wall_count;
-		}
-	return putWallspreadVirus(original_factory, empty_list, virus_list);
-}
 
 int main() {
-	cout << maxSafeZone() << endl;
+	cout << maxEmptySpace() << endl;
 }
