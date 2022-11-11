9437eb05b16b55c374c749bcf79a58d06d52cc22
reruo321 <reruo321@gmail.com>
2022-11-11 23:44:12 +0900

    committed

---
+++ Online-Judges-CPP/Baekjoon/14889/14889.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void minStatDiff() {
+	vector<vector<int>> stat_vec;
+	int n;
+	cin >> n;
+
+	stat_vec.assign(n, vector<int>(n / 2, -1));
+}
+
+int main() {
+
+	minStatDiff();
+
+	return 0;
+}
