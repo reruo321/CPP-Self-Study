560e5c2f2fc8a2bb2b42e2ab243fcf58e032b9dd
reruo321 <48712088+reruo321@users.noreply.github.com>
2023-03-29 00:01:57 +0900

    Create 17680_2.cpp

---
+++ Online-Judges-CPP/Programmers/17680/17680_2.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+using namespace std;
+
+int solution(int cacheSize, vector<string> cities) {
+    int answer = 0;
+    list<string> cli;
+    map<string, list<string>::iterator> mmap;
+    if(!cacheSize)
+        return cities.size() * 5;
+    
+    for(auto &city: cities){
+        for(auto &c: city){
+            if(c>='A'&&c<='Z')
+                c = 'a' + (c-'A');
+        }
+        
+        if(mmap.find(city)==mmap.end()){
+            if(cli.size()==cacheSize){
+                string s = cli.back();
+                cli.pop_back();
+                mmap.erase(s);
+            }
+            cli.push_front(city);
+            mmap.insert(make_pair(city, cli.begin()));
+            answer += 5;
+        }
+        else{
+            cli.erase(mmap.at(city));
+            cli.push_front(city);
+            mmap.at(city) = cli.begin();
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
