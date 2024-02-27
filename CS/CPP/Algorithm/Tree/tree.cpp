19ba351ff045af4e851103b753b496069715cf2d
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-02-27 20:42:35 +0900

    Create tree.cpp

---
+++ CS/CPP/Algorithm/Tree/tree.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class TreeNode{
+
+    TreeNode * front;
+    TreeNode * back;
+    int val;
+    
+    TreeNode(int v): val(v), front(nullptr), back(nullptr) {}
+    
+    setFront(TreeNode n){
+        front = &n;
+    }
+    setBack(TreeNode n){
+        back = &n;
+    }
+
+};
+
+class Tree{
+    
+    int traversal;
+    
+};
+
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
