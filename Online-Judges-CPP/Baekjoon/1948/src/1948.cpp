2f0dd9ac04e40b8010e351cbcf24fe5a136d1285
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-08-30 22:50:29 +0900

    Update 1948.cpp

--- Online-Judges-CPP/Baekjoon/1948/src/1948.cpp
+++ Online-Judges-CPP/Baekjoon/1948/src/1948.cpp
@@ -8,47 +8,62 @@ using namespace std;
 typedef pair<int, int> P;
 
 vector<vector<P>> graph;
+vector<int> rsum;
 vector<int> rcount;
-vector<int> topo;
-
-vector<int> roads;
+vector<int> ctopo;
 
 int f_source, f_dest;
 
 void topoFunc(int v){
-    if(topo.at(v) == 0){
-        --topo.at(v);
-        for(auto &np: graph){
+    if(ctopo.at(v) == 0){
+        --ctopo.at(v);
+        for(auto &np: graph.at(v)){
             int &next = np.first;
-            int &ri = np.second;
+            int &dist = np.second;
             
-            bool ismaximum = topoFunc()
+            --ctopo.at(next);
+            
+            cout << "? => " << v << " " << next << endl;
+            
+            int distsum = rsum.at(v) + dist;
+            if(rsum.at(next) < distsum){
+                rsum.at(next) = distsum;
+                rcount.at(next) = rcount.at(v) + 1;
+            }
+            else if(rsum.at(next) == distsum){
+                rcount.at(next) += (rcount.at(v) + 1);
+            }
+            
+            topoFunc(next);
         }
     }
 }
 
 int main(){
     
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     int n, m;
     cin >> n >> m;
     
     graph.assign(n+1, vector<P>());
-    maxroads.assign(n+1, 0);
-    topo.assign(n+1, 0);
-    
-    roads.assign(m, 0);
+    rcount.assign(n+1, 0);
+    rsum.assign(n+1, 0);
+    ctopo.assign(n+1, 0);
     
     for(int i=0; i<m; ++i){
         int source, dest, d;
         cin >> source >> dest >> d;
-        graph.at(source).push_back(make_pair(dest, i));
-        
-        roads.at(i) = d;
+        graph.at(source).push_back(make_pair(dest, d));
+        ++ctopo.at(dest);
     }
     
     cin >> f_source >> f_dest;
     topoFunc(f_source);
     
-    
+    cout << rsum.at(f_dest) << "\n" << rcount.at(f_dest) << "\n";
+
     return 0;
 }
