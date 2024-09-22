52c8633f6b5e8fa44e94670d870a7b3609caca84
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-09-22 23:04:03 +0900

    Create 1854.cpp

---
+++ Online-Judges-CPP/Baekjoon/1854/src/1854.cpp
@@ -0,0 +1,27 @@
+// Not finished
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+vector<vector<P>> graph;
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    graph.assign(m+1, 0);
+    
+    for(int i=0; i<n; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        graph.at(a).push_back(make_pair(b, c));
+    }
+
+    return 0;
+}
