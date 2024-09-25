d12bd56412a45514466091d043424735083cf8d4
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-09-25 23:19:29 +0900

    Update 1854.cpp

--- Online-Judges-CPP/Baekjoon/1854/src/1854.cpp
+++ Online-Judges-CPP/Baekjoon/1854/src/1854.cpp
@@ -1,4 +1,4 @@
-// Not finished
+// Not finished. Wrong solution
 
 #include <iostream>
 #include <vector>
@@ -6,19 +6,25 @@
 
 using namespace std;
 
-typedef pair<int, int> P;
+typedef pair<long long, long long> P;
 
 vector<vector<P>> graph;
 priority_queue<P, vector<P>, greater<P>> que;
+vector<priority_queue<long long>> citydist;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     int n, m, k;
     cin >> n >> m >> k;
 
-    graph.assign(m+1, vector<P>());
+    graph.assign(n+1, vector<P>());
+    citydist.assign(n+1, priority_queue<long long>());
     
-    for(int i=0; i<n; ++i){
+    for(int i=0; i<m; ++i){
         int a, b, c;
         cin >> a >> b >> c;
         
@@ -26,6 +32,46 @@ int main()
     }
     
     que.push(make_pair(0, 1));
+    
+    while(!que.empty()){
+        P p = que.top();
+        que.pop();
+        
+        long long &vtime = p.first;
+        long long &v = p.second;
+        
+        for(auto &np: graph.at(v)){
+            long long &next = np.first;
+            long long &dist = np.second;
+            bool issmall = true;
+            
+            long long total_time = vtime + dist;
+            
+            if(citydist.at(next).size() < k){
+                citydist.at(next).push(total_time);
+            }
+            else{
+                if(total_time < citydist.at(next).top()){
+                    citydist.at(next).pop();
+                    citydist.at(next).push(total_time);
+                }
+                else{
+                    issmall = false;
+                }
+            }
+            
+            if(issmall){
+                que.push(make_pair(total_time, next));
+            }
+        }
+    }
+    
+    for(int i=1; i<=n; ++i){
+        if(citydist.at(i).size() != k)
+            cout << -1 << "\n";
+        else
+            cout << citydist.at(i).top() << "\n";
+    }
 
     return 0;
 }
