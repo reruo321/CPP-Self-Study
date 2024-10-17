a3b2efaf2ec00ca88d0349ca1ae3fd7264091ea6
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-10-18 00:39:53 +0900

    Update 1219.cpp

--- Online-Judges-CPP/Baekjoon/1219/src/1219.cpp
+++ Online-Judges-CPP/Baekjoon/1219/src/1219.cpp
@@ -1,5 +1,3 @@
-// Not finished, wrong solution
-
 #include <iostream>
 #include <vector>
 #include <tuple>
@@ -8,6 +6,7 @@
 using namespace std;
 
 const long long LLONG_MAX = numeric_limits<long long>::max();
+const long long LLONG_MIN = numeric_limits<long long>::min();
 
 vector<tuple<int, int, int>> edges;
 vector<long long> vertices;
@@ -30,7 +29,7 @@ int main()
         edges.at(i) = {a, b, c};
     }
     
-    vertices.assign(N, LLONG_MAX);
+    vertices.assign(N, LLONG_MIN);
     credits.assign(N, 0);
     
     for(int i=0; i<N; ++i){
@@ -58,28 +57,7 @@ int main()
             int s, d, cost;
             tie(s, d, cost) = t;
             
-            if(vertices.at(s) != LLONG_MAX){
-                long long compdist = vertices.at(s) - cost + credits.at(d);
-                if(vertices.at(d) == LLONG_MAX || vertices.at(d) < compdist){
-                    vertices.at(d) = compdist;
-                }
-            }
-        }
-    }
-    
-    long long first_dest = vertices.at(dest);
-    
-    if(first_dest == LLONG_MAX){
-        cout << "gg\n";
-        return 0;
-    }
-
-    for(int i=0; i<N-1; ++i){
-        for(auto &t: edges){
-            int s, d, cost;
-            tie(s, d, cost) = t;
-            
-            if(vertices.at(s) != LLONG_MAX){
+            if(vertices.at(s) != LLONG_MIN){
                 long long compdist = vertices.at(s) - cost + credits.at(d);
                 if(vertices.at(d) < compdist){
                     vertices.at(d) = compdist;
@@ -88,7 +66,33 @@ int main()
         }
     }
     
-    if(first_dest >= vertices.at(dest)){
+    if(vertices.at(dest) == LLONG_MIN){
+        cout << "gg\n";
+        return 0;
+    }
+    
+    long long first_dest = vertices.at(dest);
+    
+    for(int i=0; i<N-1; ++i){
+        for(auto &t: edges){
+            int s, d, cost;
+            tie(s, d, cost) = t;
+            
+            if(vertices.at(s) != LLONG_MIN){
+                if(vertices.at(s) == LLONG_MAX){
+                    vertices.at(d) = LLONG_MAX;
+                }
+                else{
+                    long long compdist = vertices.at(s) - cost + credits.at(d);
+                    if(vertices.at(d) < compdist){
+                        vertices.at(d) = LLONG_MAX;
+                    }
+                }
+            }
+        }
+    }
+    
+    if(vertices.at(dest) != LLONG_MAX){
         cout << first_dest << "\n";
     }
     else{
