7cec78f59755d5f9c8b654bdb7a0355597145cfe
reruo321 <48712088+reruo321@users.noreply.github.com>
2024-10-26 23:31:15 +0900

    Update 11404.cpp

--- Online-Judges-CPP/Baekjoon/11404/src/11404.cpp
+++ Online-Judges-CPP/Baekjoon/11404/src/11404.cpp
@@ -1,1 +1,27 @@
+// Not finished
 
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+vector<pair<int, int>> graph;
+vector<vector<int>> floyd;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    
+    graph.assign(n+1, pair<int, int>());
+    floyd.assign(n+1, vector<int>(n+1, 0));
+    
+    for(int i=0; i<m; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph.at(a).push_back({b, c});
+    }
+
+    return 0;
+}
