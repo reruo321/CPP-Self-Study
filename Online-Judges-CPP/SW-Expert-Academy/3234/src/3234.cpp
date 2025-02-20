99ae90e9fdc5af5b4ba1ce51712ac28f3150a968
reruo321 <48712088+reruo321@users.noreply.github.com>
2025-02-21 00:59:43 +0900

    Update 3234.cpp

--- Online-Judges-CPP/SW-Expert-Academy/3234/src/3234.cpp
+++ Online-Judges-CPP/SW-Expert-Academy/3234/src/3234.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+int N;
+int answer;
+int v_input[10];
+
+void putWeight(int level, int sum) {
+
+	if (level == N) {
+		++answer;
+		return;
+	}
+
+	int &w = v_input[level];
+
+	putWeight(level + 1, sum + w);
+	if (sum >= w)
+		putWeight(level + 1, sum - w);
+
+}
+
+void choo() {
+	do {
+		putWeight(0, 0);
+	} while (next_permutation(v_input, v_input+N));
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+	int T;
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (int test_case = 1; test_case <= T; ++test_case) {
+		scanf("%d", &N);
+		answer = 0;
+
+		for (int i = 0; i < N; ++i) {
+			scanf("%d", &v_input[i]);
+		}
+
+		sort(v_input, v_input+N);
+		choo();
+
+		printf("#%d %d\n", test_case, answer);
+	}
+
+	return 0;
+}
