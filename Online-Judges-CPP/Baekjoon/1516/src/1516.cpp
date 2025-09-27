92d84de80566ddb7d0313426dba37e1e6d850788
reruo321 <reruo321@gmail.com>
2025-09-28 05:47:05 +0900

    Committed 1516

--- Online-Judges-CPP/Baekjoon/1516/src/1516.cpp
+++ Online-Judges-CPP/Baekjoon/1516/src/1516.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<int> topo;
+vector<int> initcost;
+vector<int> finalcost;
+
+void topoFunc(int a){
+    if(topo.at(a) == 0){
+        finalcost.at(a) += initcost.at(a);
+        --topo.at(a);
+        for(auto &next: graph.at(a)){
+            --topo.at(next);
+            
+            if(finalcost.at(next) < finalcost.at(a))
+            finalcost.at(next) = finalcost.at(a);
+            topoFunc(next);
+        }
+    }
+}
 
 int main(){
 
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    
+    graph.assign(N+1, vector<int>());
+    topo.assign(N+1, 0);
+    initcost.assign(N+1, 0);
+    finalcost.assign(N+1, 0);
+    
+    for(int i=1; i<=N; ++i){
+        int input;
+        cin >> input;
+        initcost.at(i) = input;
+        cin >> input;
+        while(input != -1){
+            graph.at(input).push_back(i);
+            ++topo.at(i);
+            cin >> input;
+        }
+    }
+    
+    for(int i=1; i<=N; ++i){
+        topoFunc(i);
+    }
+    
+    for(int i=1; i<=N; ++i){
+        cout << finalcost.at(i) << "\n";
+    }
+
+    return 0;    
 }
